e0ab3ab2225fff569aa215812e46d95d386ea00a
Vasilii Ponteleev <neocaptainnemo@gmail.com>
2017-02-02 14:48:50 +0100

    Hiding fab on scroll.

---
+++ app/src/main/java/com/neocaptainnemo/cv/ui/FabScrollBehaviour.java
@@ -0,0 +1,46 @@
+package com.neocaptainnemo.cv.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.neocaptainnemo.cv.R;
+
+public class FabScrollBehaviour extends CoordinatorLayout.Behavior<FloatingActionButton> {
+    private int toolbarHeight;
+
+    public FabScrollBehaviour(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.toolbarHeight = getToolbarHeight(context);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
+        if (dependency instanceof AppBarLayout) {
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
+            int fabBottomMargin = lp.bottomMargin;
+            int distanceToScroll = fab.getHeight() + fabBottomMargin;
+            float ratio = (float) dependency.getY() / (float) toolbarHeight;
+            fab.setTranslationY(-distanceToScroll * ratio);
+        }
+        return true;
+    }
+
+    private int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+}
--- app/src/main/java/com/neocaptainnemo/cv/ui/projects/ProjectDetailsActivity.java
+++ app/src/main/java/com/neocaptainnemo/cv/ui/projects/ProjectDetailsActivity.java
@@ -153,7 +153,6 @@ public class ProjectDetailsActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
     @Override
